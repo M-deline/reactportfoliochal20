7bd98cb656992b787925833fb39521419b2b40f8
Madeline K <madkovanda@gmail.com>
2023-11-28 12:46:57 -0700

    update and update Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,35 @@
-// WHEN I view the navigation titles
+// WHEN I view the Nav titles
 // THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
-// WHEN I click on a navigation title
-// THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
+// WHEN I click on a Nav title
+// THEN I am presented with the corresponding section below the Nav without the page reloading and that title is highlighted
+import React from "react";
+
+function Nav(props) {
+  const tabs = ["About", "Portfolio", "Contact", "Resume"];
+  return (
+    <div className="tab is-centered">
+      <ul className="nav nav-tabs">
+        {tabs.map((tab) => (
+          <li
+            className={
+              props.currentPage === tab ? "nav-item is-active" : "nav-item"
+            }
+            key={tab}
+          >
+            <a
+              href={"#" + tab.toLowerCase()}
+              onClick={() => props.handlePageChange(tab)}
+              className={
+                props.currentPage === tab ? "nav-link active" : "nav-link"
+              }
+            >
+              {tab}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;

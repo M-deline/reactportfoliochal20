e6928ecfaa95219de7bf51cfaaa73834e8fd6ae9
Madeline K <madkovanda@gmail.com>
2023-11-29 11:14:05 -0700

    update css and about me with placeholder

--- src/components/About.js
+++ src/components/About.js
@@ -4,22 +4,26 @@
 // Recent Photo or Avatar
 // Short Biography
 import React from "react";
-
+// import placeholder from '../images/placeholder.jpeg';
 function About() {
   return (
     <div>
       <h4 className="about-text">About Me</h4>
       <hr />
-      <img className="my-pic" src={process.env.PUBLIC_URL + '/img/profile.jpg'} alt=""/>
-      <p className="about-text">
-      <br></br>
-       I am Madeline and I am studying full stack web development in order to become a full time developer. I am almost finished with the FullStack Developer BootCamp from University of Denver. 
-      </p>
-      <p className="about-text">
-        <br></br>
-        <br></br>
-        Outside of the bootcamp, I love to practice coding for personal apps and connect wiht other people to practice Spanish. I love to read and go on hikes all over Colorado. 
-      </p>
+      <div className="content">
+        <img className="images" src={process.env.PUBLIC_URL + '/images/placeholder.jpeg'} alt=""/>
+        <div>
+          <p className="about-text">
+            <br></br>
+            I am Madeline and I am studying full stack web development in order to become a full time developer. I am almost finished with the FullStack Developer BootCamp from University of Denver. 
+          </p>
+          <p className="about-text">
+            <br></br>
+            <br></br>
+            Outside of the bootcamp, I love to practice coding for personal apps and connect with other people to practice Spanish. I love to read and go on hikes all over Colorado. 
+          </p>
+        </div>
+      </div>
       <br></br>
       <br></br>
     </div>

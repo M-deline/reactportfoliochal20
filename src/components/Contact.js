8f407c9ce8a1aceebe7ea87a0713d1d013617a41
Madeline K <madkovanda@gmail.com>
2023-11-28 19:26:27 -0700

    update contact form to actually be able to email me a message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,81 +4,59 @@
 // THEN I receive a notification that this field is required
 // WHEN I enter text into the email address field
 // THEN I receive a notification if I have entered an invalid email address
-
+//https://formspree.io/f/xwkddnnv
 import React, { useState } from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-import { validateEmail } from '../utils/helpers';
-// import Nav from './components/Nav';
-
 
 function Contact() {
-  const [state, handleSubmit] = useForm(process.env.REACT_APP_FORM_ID);
-  console.log(process.env.REACT_APP_MY_VARIABLE);
-
-  const [formState, setFormState] = useState({ Name: '', Email: '', Message: '' });
-
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-
-
-  if (state.succeeded) {
-    return (
-      <div>
-        <p>Thank you</p>
-      </div>
-    );
-}
-
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
   const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Error: Please enter a valid email.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`Error: ${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    fetch('https://formspree.io/f/xwkddnnv', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formState),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      setFormState({
+        name: '',
+        email: '',
+        message: '',
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   };
 
   return (
-    <div>
-      <p className="content is-medium">Contact Me</p>
-      <hr />
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div className="field">
-          <label className="label" htmlFor="name">Your Name</label>
-          <input className="input" type="text" name="name" defaultValue={name} onBlur={handleChange} />
-          <ValidationError prefix="Name" field="name" errors={state.errors} />
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="email">Your Email Address</label>
-          <input className="input" type="email" name="email" defaultValue={email} onBlur={handleChange} />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="message">Write Your Message</label>
-          <textarea className="textarea" name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-          <ValidationError prefix="Message" field="message" errors={state.errors} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="is-danger">{errorMessage}</p>
-          </div>
-        )}
-        <button className="button is-medium is-primary is-fullwidth" data-testid="button" type="submit">Send</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <p>Name</p>
+      <input type="text" name="name" onChange={handleChange} />
+      <p>Email</p>
+      <input type="email" name="email" onChange={handleChange} />
+      <p>Write your message:</p>
+      <textarea name="message" onChange={handleChange} />
+      <button type="submit">Submit</button>
+    </form>
   );
-        }
+}
+
 export default Contact;
+
